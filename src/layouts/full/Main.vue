ba9536c6a99dd455e55fd58e91e4de4ee8b397a5
Hiboboo <loveboboo@vip.qq.com>
2024-10-09 23:39:27 +0800

    基本效果已实现

--- src/layouts/full/Main.vue
+++ src/layouts/full/Main.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, shallowRef } from 'vue';
+import { ref, shallowRef, watch, onMounted, onBeforeUnmount } from 'vue';
 import sidebarItems from './vertical-sidebar/sidebarItem';
 import NavGroup from '@/layouts/full/vertical-sidebar/NavGroup/index.vue'
 import NavItem from '@/layouts/full/vertical-sidebar/NavItem/index.vue'
@@ -9,13 +9,40 @@ import { Menu2Icon, BellRingingIcon } from 'vue-tabler-icons';
 import NotificationDD from './vertical-header/NotificationDD.vue';
 import ProfileDD from './vertical-header/ProfileDD.vue';
 const sidebarMenu = shallowRef(sidebarItems);
-const sDrawer = ref(true);
+const drawerWidth = ref(270);
+const sDrawer = ref(false);
 
+watch([sDrawer, drawerWidth], ([open, width]) => {
+  document.documentElement.style.setProperty('--drawer-width', !open ? `${width}px` : '56px')
+})
+
+const isSticky = ref(false);
+const appBarTop = ref(20);
+const initialTop = ref(20);
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
+
+function handleScroll() {
+    const currentScrollPosition = window.scrollY;
+    if (currentScrollPosition <= initialTop.value) {
+        isSticky.value = false;
+        appBarTop.value = initialTop.value;
+    } else {
+        isSticky.value = true;
+        appBarTop.value = 0;
+    }
+}
 </script>
 
 <template>
-    <v-navigation-drawer left v-model="sDrawer" app class="leftSidebar ml-sm-5 mt-sm-5 bg-containerBg" elevation="10"
-        width="270">
+    <v-navigation-drawer left :rail="sDrawer" app class="leftSidebar ml-sm-5 mt-sm-5 bg-containerBg" elevation="10"
+        :width="drawerWidth">
         <div class="pa-5 pl-4 ">
             <Logo />
         </div>
@@ -34,19 +61,19 @@ const sDrawer = ref(true);
                 </template>
                 <!-- <Moreoption/> -->
             </v-list>
-            <div class="py-0 px-6">
+            <!-- <div class="py-0 px-6">
                 <v-btn class="mr-2 bg-primary rounded-pill" size="large"
                     href="https://www.wrappixel.com/templates/spike-vuejs-admin-dashboard/?ref=33" block target="_blank">Upgrade to
                     Pro</v-btn>
-            </div>
+            </div> -->
         </perfect-scrollbar>
     </v-navigation-drawer>
     <div class="container verticalLayout">
         <div class="maxWidth">
-            <v-app-bar elevation="0" height="70">
+            <v-app-bar elevation="0" height="75" :class="{ 'sticky-top': isSticky }" :style="{ top: appBarTop + 'px' }">
                 <div class="d-flex align-center justify-space-between w-100">
                     <div>
-                        <v-btn class="hidden-lg-and-up text-muted" @click="sDrawer = !sDrawer" icon
+                        <v-btn class="text-muted" @click="sDrawer = !sDrawer" icon
                             variant="flat" size="small">
                             <Menu2Icon size="20" stroke-width="1.5" />
                         </v-btn>
@@ -55,9 +82,9 @@ const sDrawer = ref(true);
                     </div>
                     <div>
                         <!-- Upgrade button -->
-                        <v-btn class="mr-2 bg-primary rounded-pill"
+                        <!-- <v-btn class="mr-2 bg-primary rounded-pill"
                             href="https://www.wrappixel.com/templates/spike-vuejs-admin-dashboard/?ref=33"
-                            target="_blank">Upgrade to Pro</v-btn>
+                            target="_blank">Upgrade to Pro</v-btn> -->
                         <!-- User Profile -->
                         <ProfileDD />
                     </div>
@@ -66,3 +93,14 @@ const sDrawer = ref(true);
         </div>
     </div>
 </template>
+<style scoped>
+.v-app-bar {
+  transition: top 0.3s ease;
+}
+.sticky-top {
+  position: fixed;
+  left: calc(var(--drawer-width) + 40px) !important;
+  width: calc(100% - var(--drawer-width) - 96px) !important;
+  right: 56px !important;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import App from './App.vue'
 import { router } from './router'
 import vuetify from './plugins/vuetify'
 import '@/scss/style.scss'
-import PerfectScrollbar from 'vue3-perfect-scrollbar';
+import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar';
 import VueApexCharts from 'vue3-apexcharts';
 import VueTablerIcons from 'vue-tabler-icons';
-import Maska from 'maska';
+// import Maska from 'maska';
 
 const app = createApp(App)
 app.use(createPinia())
     .use(router)
-    .use(PerfectScrollbar)
+    .use(PerfectScrollbarPlugin)
     .use(VueTablerIcons)
-    .use(Maska)
+    // .use(Maska)
     .use(VueApexCharts)
     .use(vuetify)
 app.mount('#app')
