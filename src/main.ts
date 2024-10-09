e172b4ae69f0087ae4143a6c837248347a9b6479
sunbo <bsun@jinkeen.com>
2024-10-09 17:26:53 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import { router } from './router'
+import vuetify from './plugins/vuetify'
+import '@/scss/style.scss'
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
+import VueApexCharts from 'vue3-apexcharts';
+import VueTablerIcons from 'vue-tabler-icons';
+import Maska from 'maska';
+
+const app = createApp(App)
+app.use(createPinia())
+    .use(router)
+    .use(PerfectScrollbar)
+    .use(VueTablerIcons)
+    .use(Maska)
+    .use(VueApexCharts)
+    .use(vuetify)
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vuetify from 'vite-plugin-vuetify'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vuetify({
+      autoImport: true,
+      styles: { configFile: 'src/scss/variables.scss' }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
+  optimizeDeps: {
+    exclude: ['vuetify'],
+    entries: ['./src/**/*.vue']
+  }
+})
